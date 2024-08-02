96b7118de6ffab0ccc79ac49071c41ac2c303261
Angel Leon <angel.leon.ram@gmail.com>
2024-08-02 12:12:28 -0300

    Bento progression, code optimization

--- src/Pages/Home/sections/ProductsHome_section2.jsx
+++ src/Pages/Home/sections/ProductsHome_section2.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+// // IMPORTS FROM PANADERIA SECTION
+import bizcochos from '../../../assets/Images/Panaderia/bizcochos.jpg'
+import pan from '../../../assets/Images/Panaderia/pan.jpg'
+
+// // // IMPORTS FROM ROTISERIA SECTION
+import bauru from '../../../assets/Images/Rotiseria/bauru.jpg'
+import bocata from '../../../assets/Images/Rotiseria/bocata.jpg'
+
+// // // IMPORTS FROM CONFITERIA SECTION
 import masitas from '../../../assets/Images/Confiteria/masitas.jpg'
 import foleatela from '../../../assets/Images/Confiteria/foleatela.jpg'
 import dona from '../../../assets/Images/Confiteria/dona.jpg'
@@ -6,6 +14,9 @@ import rosquita from '../../../assets/Images/Confiteria/rosquita.jpg'
 import tortaDulce from '../../../assets/Images/Confiteria/torta_dulce_leche.jpg'
 import palitosQueso from '../../../assets/Images/Confiteria/palitos_de_queso.jpg'
 import chaja from '../../../assets/Images/Confiteria/chaja.jpg'
+
+// // // IMPORTS FROM SANDWICHERIA SECTION
+
 
 const ProductsHome_section2 = () => {
   return (
@@ -16,14 +27,9 @@ const ProductsHome_section2 = () => {
           <h2>Panadería</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aperiam a alias vitae doloremque quis recusandae minus assumenda ipsam quae.</p>
         </div>
-        <div>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+        <div className='panaderiaBento'>
+          <img src={bizcochos} alt="" />
+          <img src={pan} alt="" />
         </div>
       </div>
       <div className='rotiseriaContainer'>
@@ -31,14 +37,9 @@ const ProductsHome_section2 = () => {
           <h2>Rotisería</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fugiat, dolorem aliquid eaque deserunt, explicabo voluptatibus exercitationem dignissimos nobis nemo laboriosam perferendis ex quibusdam aut?</p>
         </div>
-        <div>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+        <div className='rotiseriaBento'>
+          <img src={bauru} alt="" />
+          <img src={bocata} alt="" className='bocataImg' />
         </div>
       </div>
       <div className='confiteriaContainer'>
@@ -46,7 +47,7 @@ const ProductsHome_section2 = () => {
           <h2>Confitería</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fugiat, dolorem aliquid eaque deserunt, explicabo voluptatibus exercitationem dignissimos nobis nemo laboriosam perferendis ex quibusdam aut?</p>
         </div>
-        <div className='confiteriaBento'>
+        <div className='confiteriaBento '>
           <div className='fatherLeftDiv'>
             <div className='childrenLeftDiv'>
               <img src={masitas} alt="" className='topLeftImg' />
