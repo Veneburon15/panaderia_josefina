f6812cf23833ebde24ba6c091bb8897c386d1773
Angel Leon <angel.leon.ram@gmail.com>
2024-08-11 00:58:13 -0300

    RecommendedLunch Component finalization, CustomLunch Component initialization

--- src/Pages/LunchService/sections/CustomLunch.jsx
+++ src/Pages/LunchService/sections/CustomLunch.jsx
@@ -2,7 +2,11 @@ import React, { forwardRef } from 'react'
 
 const CustomLunch = ({}, customSection) => {
   return (
-    <section ref={customSection} className='customLunch'>CustomLunch</section>
+    <section ref={customSection} className='customLunch'>
+      <h2>Crea tu lunch perfecto</h2>
+      <h3>¿Tienes una idea pero no sabes cuanto puede costar?</h3>
+      <p>Has un presupuesto con nosotros</p>
+    </section>
   )
 }
 
